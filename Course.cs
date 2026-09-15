600a31299df9f722d491564faf2e347e4a618ff2
Nikolai Sneckenborg <nikolai@simbalsyd.se>
2026-09-15 15:23:45 +0200

    La till ett antal kurser I program för att testa och gjorde en Enroll funktion

--- Course.cs
+++ Course.cs
@@ -4,4 +4,15 @@ class Course(string name, int maxSeats)
     public string Name = name;
     public int MaxSeats = maxSeats;
 
+    public void Enroll(Student student)
+    {
+        if (!students.Contains(student))
+        {
+            students.Add(student);
+        }
+        else
+        {
+            Console.WriteLine("Studenten är redan anmäld till kursen!");
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿Student[] students = [new ("Kalle"), new ("Pelle"), new ("Olle")];
+Course[] courses = [new ("Math", 50), new ("Biology", 40), new ("Chemistry", 28)];
+
 
 foreach(Student student in students)
 {
     Console.WriteLine(student.Name);
+}
+
+foreach(Course course in courses)
+{
+    Console.WriteLine($"Course: {course.Name} - Seats: {course.MaxSeats}");
 }
