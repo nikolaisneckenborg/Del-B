5e22e77655e922718494f807b9f443f44ca6326a
Nikolai Sneckenborg <nikolai@simbalsyd.se>
2026-09-15 15:10:42 +0200

    Skapade lista och testade skriva ut studenter i Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Student[] students = [new ("Kalle"), new ("Pelle"), new ("Olle")];
+
+foreach(Student student in students)
+{
+    Console.WriteLine(student.Name);
+}
